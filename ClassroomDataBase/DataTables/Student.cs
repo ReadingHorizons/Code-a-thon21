f8a2d1b84dfd37384951f66d2830fa29fbb977cd
Joshua Eversole <joshua.eversole@readinghorizons.com>
2021-06-14 15:32:42 -0600

    Add Student Class

---
+++ ClassroomDataBase/DataTables/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassroomDataBase.DataTables
+{
+    class Student
+    {
+        public int Id { get; set; }
+        public String FirstName { get; set; }
+        public String LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public int ClassID { get; set; }
+        public int Gender { get; set; }
+    }
+}
