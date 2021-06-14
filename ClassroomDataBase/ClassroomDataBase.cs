1a8debbf014ac26f3394b40b1d00507c081a9082
Melissa <melissa.prisbrey@readinghorizons.com>
2021-06-14 15:21:44 -0600

    Added Stuff

---
+++ ClassroomDataBase/ClassroomDataBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+namespace ClassroomDataBase
+{
+    public class ClassroomDataBase: DbContext
+    {
+        public virtual DbSet<Teacher> Teachers { get; set; } 
+    }
+}
---
+++ ClassroomDataBase/DataTables/Teacher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassroomDataBase.DataTables
+{
+    public class Teacher
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int ClassId { get; set; }
+    }
+}
