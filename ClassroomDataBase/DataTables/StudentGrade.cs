6dfa492e6f1c83a2de67d7be121bd014d434a530
Joshua Eversole <joshua.eversole@readinghorizons.com>
2021-06-14 15:34:02 -0600

    Added StudentGrade Class

---
+++ ClassroomDataBase/DataTables/StudentGrade.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassroomDataBase.DataTables
+{
+    class StudentGrade
+    {
+        public int Id { get; set; }
+        public int StudentId { get; set; }
+        public int AssignmentId { get; set; }
+        public float Grade { get; set; }
+    }
+}
