ec33d943c5e2f791d06f0ac3545a0ddc7df615ef
Melissa <melissa.prisbrey@readinghorizons.com>
2021-06-14 15:47:24 -0600

    Added classes

--- ClassroomDataBase/ClassroomDataBase.cs
+++ ClassroomDataBase/ClassroomDataBase.cs
@@ -4,11 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.Entity;
+using System.Configuration;
+using ClassroomDataBase.DataTables;
 
 namespace ClassroomDataBase
 {
     public class ClassroomDataBase: DbContext
     {
+        private static string connectionString = ConfigurationManager.ConnectionStrings["ClassroomDatabase"].ConnectionString;
+        public ClassroomDataBase()
+        {
+            this.Database.Connection.ConnectionString = connectionString;
+        }
         public virtual DbSet<Teacher> Teachers { get; set; } 
+        public virtual DbSet<Student> Students { get; set; } 
+        public virtual DbSet<Assignment> Assignments { get; set; } 
+        public virtual DbSet<Class> Classes { get; set; } 
+        public virtual DbSet<StudentGrade> StudentGrades { get; set; } 
     }
 }
